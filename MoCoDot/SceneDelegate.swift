ed8f154f2be1f925d574aa65d428790ce4e446c1
jakkujakku <jakkujakku98@gmail.com>
2024-01-11 00:55:32 +0900

    init: MoCoDot 프로젝트 생성

---
+++ MoCoDot/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  MoCoDot
+//
+//  Created by 준우의 MacBook 16 on 1/11/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        self.window = UIWindow(windowScene: windowScene)
+
+        let rootViewController = ViewController()
+        let rootNavigationController = UINavigationController(rootViewController: rootViewController)
+
+        self.window?.rootViewController = rootNavigationController
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
---
+++ MoCoDot/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MoCoDot
+//
+//  Created by 준우의 MacBook 16 on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
