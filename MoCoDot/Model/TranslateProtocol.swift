e3a2c548215fda8e8f7d6da6c2445b5398d2a5b8
jakkujakku <jakkujakku98@gmail.com>
2024-02-08 14:00:49 +0900

    [FEAT]: Model && Helpers && Resources  폴더 생성 및 의존성 주입 컨테이너 프로토콜 생성
    1. Model & Helpers & Resources 폴더 생성
    
    2. 의존성 주입 컨테이너 프로토콜 생성 및 의존성 주입 컨테이너 객체 생성
    
    Resolves: none

---
+++ MoCoDot/Model/TranslateProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  TranslateProtocol.swift
+//  MoCoDot
+//
+//  Created by 준우의 MacBook 16 on 2/8/24.
+//
+
+import Foundation
--- MoCoDot/Resources/AppDelegate.swift
+++ MoCoDot/Resources/AppDelegate.swift
@@ -9,8 +9,10 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    let container = DIContainer.shared
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+//        container.register(type: <#T##T.Type#>, service: <#T##Any#>)
         return true
     }
 
