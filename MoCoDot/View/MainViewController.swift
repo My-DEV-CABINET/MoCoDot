b099ec93aee85998a34905f7240eaac37a1d3f97
jakkujakku <jakkujakku98@gmail.com>
2024-02-08 13:54:15 +0900

    [FEAT]: ViewController 이름 변경 및 MainViewModel 생성
    1. ViewController 이름 변경
    [BEFORE]
    - ViewController
    [AFTER]
    - MainViewController
    
    2. MainViewModel 생성
    
    Resolves: none

--- MoCoDot/View/MainViewController.swift
+++ MoCoDot/View/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MoCoDot
 //
 //  Created by 준우의 MacBook 16 on 1/11/24.
@@ -7,7 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
+    var viewModel: MainViewModel!
+}
+
+extension MainViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
---
+++ MoCoDot/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  MoCoDot
+//
+//  Created by 준우의 MacBook 16 on 2/8/24.
+//
+
+import Foundation
+
+final class MainViewModel {
+    
+}
